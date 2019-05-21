2058b899f7e98f576a72fefd6ee186f5ddd18d9b
FruechteBini <blanz@mailbox.org>
2019-05-21 17:06:22 +0200

    kommentare, knopfdruck

--- start_fotobox.py
+++ start_fotobox.py
@@ -4,14 +4,19 @@
 import serial
 from PIL import Image
 import os, time, sys, random
-from os.path import isfile, join
-import pygame
 from time import sleep
 from picamera import PiCamera
 
 #Bibliothek für Printer:
 from Adafruit_Thermal import *
 from os import listdir
+
+import RPi.GPIO as GPIO
+
+# Definiere Knopf:
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_UP) 
 
 #initialize camera
 camera=PiCamera(resolution = (1920, 1080))
@@ -25,47 +30,50 @@ photoResize = 512, 384
 photoTitle = "Projekt " + time.strftime("%Y-%m-%d-%H-%M") + "\n Magga"
 gewinnerTitle = " "
 
-#define printer
+#define printer (19200 ist die baudrate (siehe tutorial))
 printer = Adafruit_Thermal("/dev/serial0", 19200, timeout=5)
 
-#start camera preview
+#start camera preview (x,y,pixel,pixel)
 camera.start_preview(fullscreen=False, window=(0,0,800,480))
 
-#init pygame for mouseclick (to take foto - ersetzbar durch Knopfdruck)
-pygame.init()
+# take picture
+def take_photo():
 
-# take picture & save to photoPath (see variabled)
-def photo_callback():
-
-    # Define filename with timestamp
+    # Dateiname mit Zeitstempel:
     photoName = time.strftime("%Y-%m-%d-%H-%M-%S") + "_fotobox.jpg"
     
-    #take a picture
+    #Mache das Foto, speichere in /home/pi/photobox/fotos/DATUM_fotobox.jpg
     camera.capture(photoPath+photoName)
 
-    # Resize the high res photo to create thumbnail
+    # RGERADE gemachtes Foto öffnen, kleiner machen , sichern als "thumbnail.jpg" (wird immer überschrieben)
     Image.open(photoPath + photoName).resize(photoResize, Image.ANTIALIAS).save(photoPath + "thumbnail.jpg")
 
 
-# open the picture taken in photo_callback & print it using the adafruit printer.
-def print_callback():
-        # Rotate the thumbnail for printing
+# Foto (thumbnail.jpg) wird geöffnet, gedreht, und gedruckt
+def print_photo():
+        # Foto öffnen, drehen, sichern als "thumbnail.rotated.jpg"
         Image.open(photoPath + "thumbnail.jpg").transpose(2).rotate(180).save(photoPath + "thumbnail-rotated.jpg")
-        # Print the foto
+        # Print the foto (thumbbnail.rotated)
         printer.begin(90)                       # Warmup time
         printer.setTimes(40000, 3000)           # Set print and feed times
-        printer.justify('C')                    # Center alignment
+        printer.justify('C')                    # Zentrieren (für text und foto aufm papier)
+
+        #drucke fototitel
         printer.println(photoTitle)
+
+        #drucke foto (öffne das thumbnail-rotated)
         printer.printImage(Image.open(photoPath + "thumbnail-rotated.jpg"), True)   # Specify image to print
+        
+        #eine leere zeile:
         printer.feed(1)
 
 
 
-#MAIN Dauerschleife
+#MAIN Dauerschleife, warte auf Knopfdruck
 while True: 
-    #WARTE auf ein Ereignis:
-    for event in pygame.event.get():        
-        if(event.type == pygame.MOUSEBUTTONDOWN):   # WENN Knopf gedrückt wird
-            photo_callback()        #DANN mache Foto
-            print_callback()        #DANN druck das ganze
-            pygame.event.clear()    #lösche alles, beginne wieder mit warten auf Knopfdruck
+
+    # WENN Knopfdruck:
+    if not GPIO.input(16):
+        take_photo()        #DANN mache Foto
+        print_photo()        #DANN druck das ganze
+
